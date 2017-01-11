bd9507f1cdbc30875c914db9129281d1c4e88aef
Ermabo <marcus.bogren@gmail.com>
2017-01-11 15:48:18 +0100

    Småfix

--- MargaritaGOL/MargaritaGOL/Form1.cs
+++ MargaritaGOL/MargaritaGOL/Form1.cs
@@ -12,42 +12,41 @@ namespace MargaritaGOL
 {
     public partial class Form1 : Form
     {
+        private int cellWidth;
+        private int cellHeight;
+        //bool DOA;       Borde göra en True False istället för " Dead/Alive "
+        private int nrOfColumns;
+        private int nrOfRows;
+        string[,] GridCell;
         public Form1()
         {
+            cellWidth = 20;
+            cellHeight = 20;
+            nrOfColumns = 14;
+            nrOfRows = 13;
+            GridCell = new string[nrOfColumns, nrOfRows];
             InitializeComponent(); 
         }
-        /// <summary>
-        /// Sätter storleken på Matrisen och storleken på själva cellen
-        /// </summary>
-        static int Colums = 30;
-        static int Rows = 20;
-        static int Depth = 3;
-
-        int CellWidth = 20;
-        int Cellhight = 20;
-        bool DOA;       //Borde göra en True False istället för " Dead/Alive "
-
-        /// <summary>
-        /// Två dimensionell array som cellerna ska plaseras i, Skapar även en panel som allt händer på
-        /// </summary>
-        string[,,] GridCell = new string[Colums, Rows, Depth];
+ 
+        
+                
         Panel Panel1 = new Panel();
 
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Controls.Add(Panel1);
             Panel1.Location = new Point(0, 0);
-            Panel1.Size = new Size(CellWidth * Colums, Cellhight * Rows);
+            Panel1.Size = new Size(cellWidth * nrOfColumns, cellHeight * nrOfRows);
             Panel1.Visible = true;
 
-            for (int i = 0; i < Rows; i++)
+            for (int i = 0; i < nrOfRows; i++)
             {
-                for (int j = 0; j < Colums; j++)
+                for (int j = 0; j < nrOfColumns; j++)
                 {
                     Button cell = new Button();
-                    GridCell[j, i, 0] = "Dead";
-                    cell.Location = new Point((j * CellWidth), (i * Cellhight));
-                    cell.Size = new Size(CellWidth, Cellhight);
+                    GridCell[j, i] = "Dead";
+                    cell.Location = new Point((j * cellWidth), (i * cellHeight));
+                    cell.Size = new Size(cellWidth, cellHeight);
                     cell.BackColor = Color.White;
 
                     cell.Click += Cell_Click;
@@ -59,27 +58,27 @@ namespace MargaritaGOL
         private void Cell_Click(object sender, EventArgs e)
         {
             Button CellClick = ((Button)sender);
-            int xI = (int)(CellClick.Location.X / CellWidth);
-            int yI = (int)(CellClick.Location.Y / Cellhight);
+            int xI = (int)(CellClick.Location.X / cellWidth);
+            int yI = (int)(CellClick.Location.Y / cellHeight);
 
             if (CellClick.BackColor == Color.White)
             {
                 CellClick.BackColor = Color.Black;
-                GridCell[xI, yI, 0] = "Alive";
+                GridCell[xI, yI] = "Alive";
             }
             else
             {
                 CellClick.BackColor = Color.White;
-                GridCell[xI, yI, 0] = "Dead";
+                GridCell[xI, yI] = "Dead";
             }
             //throw new NotImplementedException();
         }
 
        public void NeighbourCells()
         {
-            for (int i = 0; i < Rows; i++)
+            for (int i = 0; i < nrOfRows; i++)
             {
-                for (int j = 0; j < Colums; j++)
+                for (int j = 0; j < nrOfColumns; j++)
                 {
                     int neighbourCells = 0;
 
@@ -87,13 +86,13 @@ namespace MargaritaGOL
                     {
                         for (int l = j - 1; l < j + 2; l++)
                         {
-                            if (k < 0  || k == Rows)
+                            if (k < 0  || k == nrOfRows)
                             {
                                 continue;
                             }
                             
                         }
-                        GridCell[j, i, 1] = neighbourCells.ToString();
+                        GridCell[j, i] = neighbourCells.ToString();
                     }
                 }
             }
