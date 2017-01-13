a7a70a8b3a58c4ca1ef58a4eae268ed66c762f7a
Ermabo <marcus.bogren@gmail.com>
2017-01-13 12:38:58 +0100

    dickbutt

---
+++ MargaritaGOL/MargaritaGOL/CellState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MargaritaGOL
+{
+    public class CellState
+    {
+        public bool IsAlive { get; set; }
+
+        public int XCord { get; set; }
+        public int YCord { get; set; }
+        public int Neighbours { get; set; }
+        public CellState()
+        {
+            this.IsAlive = false;
+            Neighbours = 0;
+        }
+    }
+}
